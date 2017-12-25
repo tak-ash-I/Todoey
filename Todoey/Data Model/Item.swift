dee978742f1e8799c3d1a807a74e5fabe08638b9
Takashi Maeda <myemail@example.com>
2017-12-26 06:44:24 +0900

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Takashi Maeda on 2017/12/26.
+//  Copyright © 2017年 mycompany. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
